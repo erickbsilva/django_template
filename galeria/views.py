c07ce12fc5f38ee4a12ac0e7ead592437b85d6ff
Erick <erickbarretodasilva@gmail.com>
2025-01-06 00:24:16 -0300

    melhorando o painel

--- galeria/views.py
+++ galeria/views.py
@@ -5,7 +5,8 @@ from galeria.models import Fotografia
 def index(request):
 
     # fotografias = Fotografia.objects.all()
-    fotografias = Fotografia.objects.filter(publicada=True)
+    # fotografias = Fotografia.objects.filter(publicada=True)
+    fotografias = Fotografia.objects.order_by("-data_fotografia").filter(publicada=True)
 
     return render(request, "galeria/index.html", {"cards": fotografias})
 
